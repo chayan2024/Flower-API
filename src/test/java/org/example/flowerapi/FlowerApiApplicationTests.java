b3669eb45cc1f7ef9dc0ed071caa5b8ea27643f2
chayan <chayanchowdhury@gmail.com>
2024-05-20 17:19:15 +0530

    Initial commit

---
+++ src/test/java/org/example/flowerapi/FlowerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.flowerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
